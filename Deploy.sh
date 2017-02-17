bb36366fbde6aca87e60f8cf43047a0aabdad1fa
Dinwy <whix2010@gmail.com>
2017-02-18 02:38:50 +0900

    Update

--- Deploy.sh
+++ Deploy.sh
@@ -22,4 +22,6 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public
+git add .
+git commit -m "Publishing to gh-pages (publish.sh)"
