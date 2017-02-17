63553c98665006d24bfff611958c56e207061901
Dinwy <whix2010@gmail.com>
2017-02-18 02:41:29 +0900

    Update

--- Deploy.sh
+++ Deploy.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B master public upstream/master
-
-echo "Removing existing files"
-rm -rf public/*
-
 echo "Generating site"
 hugo
 
@@ -25,3 +7,4 @@ echo "Updating gh-pages branch"
 cd public
 git add .
 git commit -m "Publishing to gh-pages (publish.sh)"
+git push upstream master
