59d936f7866b655664e3bae2c51f6a03ef0bf7cf
Dinwy <whix2010@gmail.com>
2017-02-18 02:12:18 +0900

    Update hugo 2017년 02월 18일 토 오전  2:12:18

--- Deploy.sh
+++ Deploy.sh
@@ -5,16 +5,19 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo
 
-# Add changes to git.
-git add -A
+git add .
+git commit -m "Update hugo `date`"
+# Push source and build repos.
+  git push origin hugo
 
+# Add changes to git.
+cd public
+git add .
+
+git push origin master
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
   fi
   git commit -m "$msg"
-
-  # Push source and build repos.
-  git push origin hugo
-  git subtree push --prefix=public git@github.com:Dinwy/dinwy.github.io.git master
