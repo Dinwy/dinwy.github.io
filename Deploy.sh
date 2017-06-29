5fa99564928698aa2c27634066d39c907fc1a8e7
Dinwy <whix2010@gmail.com>
2017-06-29 11:28:29 +0900

    Fix Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B master public upstream/master
+
+echo "Removing existing files"
+rm -rf public/*
+
 echo "Generating site"
 hugo
 git add content/
